5930bf949c0803fbcaafee06798a93afcfaebf27
James Jones <jamjon3@gmail.com>
2020-07-26 01:05:28 -0400

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# if [ "$1" = 'postgres' ]; then
+#     chown -R postgres "$PGDATA"
+
+#     if [ -z "$(ls -A "$PGDATA")" ]; then
+#         gosu postgres initdb
+#     fi
+
+#     exec gosu postgres "$@"
+# fi
+
+exec kubectl port-forward svc/mariadb-prod 3306:3306 "$@"
